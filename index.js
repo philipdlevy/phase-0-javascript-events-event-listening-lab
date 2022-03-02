9f5879639ea8ddf0db7afd113718351ab7e33d15
Philip Levy <philipdlevy@gmail.com>
2022-03-01 18:54:36 -0700

    commit tree 81487d6b19ffb4e4231fc116bace9b78fdcd9a59

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
-}
-
-const input = document.getElementById('input');
+  const input = document.getElementById('input');
 
 function clickAlert() {
   alert('I was clicked!');
 }
 
 input.addEventListener('click', clickAlert);
+}
+addingEventListener()
+
